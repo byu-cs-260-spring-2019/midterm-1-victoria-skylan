5602c1c114cd7e1d398c9cb69cc5b4617f75adfa
victoria-skylan <50216078+victoria-skylan@users.noreply.github.com>
2019-05-23 17:02:11 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,55 @@
-window.onload = function() {
-	document.getElementById("FoodSubmit").addEventListener("click", async function(event) {
-		event.preventDefault();
-		
-		const value = document.getElementById("FoodInput").value;
-		if (value === "")
-		return;
-		
-		console.log(value);
-		
-		try {
-		var type = $(this).attr("data-type");
-		var queryURL = "https://api.edamam.com/search?q=" + value + "&app_id=e01c42d8&app_key=19a34826099c7e0c9666127afe12981b";
-		console.log(queryURL);
-
-		// Grabbing our API results
-		var n=0;
-		$.ajax({
-			url: queryURL,
-			method: "GET",
-		  })
-			.then (function(response) {
-			  $(".title").text("Recipe: " + response.hits[n].recipe.label);
-			  $(".img").attr("src", response.hits[n].recipe.image);
-			  
-			  var info = "";
-			  info += "From: " + '<a target="_blank"  href="' + response.hits[n].recipe.url + '">' + response.hits[n].recipe.source + '</a><br/>';
-			  info += "Calories: " + response.hits[n].recipe.calories + "<br/>";
-			  info += "Diet Labels: ";
-			  for (let i=0; i < response.hits[n].recipe.dietLabels.length; i++) {
-					info += response.hits[n].recipe.dietLabels[i];
-				  if (i < (response.hits[n].recipe.dietLabels.length - 1) ) {
-					info += ", ";
-				  }
-				  else {
-					  info += " ";
-				  }
-			  }
-			  info += "<br/>Health Labels: ";
-			  for (let i=0; i < response.hits[n].recipe.healthLabels.length; i++) {
-					info += response.hits[n].recipe.healthLabels[i];
-				  if (i < (response.hits[n].recipe.healthLabels.length - 1) ) {
-					info += ",  ";
-				  }
-			  }
-			  document.getElementById("foodInfo").innerHTML = "<p> " + info + "</p>";
-			  
-			  var rec = "";
-			  for (let i=0; i < response.hits[n].recipe.ingredients.length; i++) {
-					rec += "- " + response.hits[n].recipe.ingredients[i].text + "<br/>";
-			  }
-			  document.getElementById("ingredientList").innerHTML = "<p> " + rec + "</p>";
-		  });
-			
-			}catch(err) {
-			console.log(err);
+let app = new Vue({
+  el: '#app',   //dom element that vue is managing
+  data: {
+	bookEntered: '',
+	currentISBN: '',
+	booknum: '',
+	allBooks: {},
+	loading: false,
+	addedName: '',
+	  
+    addedComment: '',
+    favoiteBooks: {},
+  },
+  methods: {
+    async findBooks() {
+	  try {
+		this.loading = true;
+		const bookString = this.bookEntered.replace(/ /g,"+");
+        const json = await axios.get('http://openlibrary.org/search.json?q=' + bookString);
+		for (let i=0; i < json.data.docs.length; i++) {
+			Vue.set(app.allBooks, this.i, new Array);
+			this.allBooks[this.i].push({
+					title: json.data.docs[i].title_suggest,
+					//publishDate: json.data.docs[i].
+					})
+			/*
+			if(json.data.docs[i].hasOwnProperty( 'isbn')){
+				
+				this.currentISBN = json.data.docs[i].isbn[0];
+				const response = await axios.get('https://openlibrary.org/api/books?bibkeys=ISBN:' + this.currentISBN + '&jscmd=details&format=json');
+				const thePath = "ISBN:" + this.currentISBN;
+				Vue.set(app.allBooks, this.i, new Array);
+				this.allBooks[this.i].push({
+					title: response.data.thePath.details.title,
+					publishDate: response.publish_date,
+					img: response.preview_url
+					})
+				  this.currentISBN=0;
+			}*/
 		}
-	});
-}
+		this.loading = false;
+	  } catch (error) {
+		console.log(error);
+	  }
+	},
+	searchBook() {
+		this.findBooks();
+  }  
+  },
+    computed: {
+		
+  },
+    watch: {  // will make so changing the number does something
+  },
+});
