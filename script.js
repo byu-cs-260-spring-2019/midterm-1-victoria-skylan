92512c51ea6ab6ed81ac662594d6385514f58412
victoria-skylan <50216078+victoria-skylan@users.noreply.github.com>
2019-06-04 22:07:04 -0600

    Add files via upload
    the vue is updated. Displays books now.

--- script.js
+++ script.js
@@ -2,54 +2,71 @@ let app = new Vue({
   el: '#app',   //dom element that vue is managing
   data: {
 	bookEntered: '',
-	currentISBN: '',
 	booknum: '',
-	allBooks: {},
+	booktoAd: {}, 
+	allBooks: [],
 	loading: false,
 	addedName: '',
 	  
-    addedComment: '',
-    favoiteBooks: {},
+  addedComment: '',
+  favoiteBooks: {},
   },
   methods: {
     async findBooks() {
-	  try {
-		this.loading = true;
-		const bookString = this.bookEntered.replace(/ /g,"+");
-        const json = await axios.get('http://openlibrary.org/search.json?q=' + bookString);
-		for (let i=0; i < json.data.docs.length; i++) {
-			Vue.set(app.allBooks, this.i, new Array);
-			this.allBooks[this.i].push({
+			try {
+				this.loading = true;
+				const bookString = this.bookEntered.split(' ').join('+');
+
+				const json = await axios.get('http://openlibrary.org/search.json?q=' + bookString);
+				for (let i = 0; i < json.data.docs.length; i++) {
+				console.log(i);
+				var curDate = "";
+				var curAuth = "";
+				var curIS = "";
+				if(json.data.docs[i].hasOwnProperty( 'publish_date')) {
+					curDate = json.data.docs[i].publish_date[0];
+				}
+				if (json.data.docs[i].hasOwnProperty( 'author_name')) {
+					curAuth =  "Written by: " + json.data.docs[i].author_name[0];
+				}
+				if (json.data.docs[i].hasOwnProperty('isbn') ) {
+					curIS = json.data.docs[i].isbn[0];
+					const response = await axios.get('https://openlibrary.org/api/books?bibkeys=ISBN:' + curIS + '&jscmd=details&format=json');
+					if (response.data['ISBN:'+ curIS].hasOwnProperty('thumbnail_url')) {
+						curIS = response.data['ISBN:'+ curIS].thumbnail_url;
+					}
+					else {
+						curIS = "https://i.pinimg.com/originals/b5/48/c4/b548c4b1da24ac7ec74785b4e4581d7f.jpg";
+					}
+				} else {
+					curIS = "https://i.pinimg.com/originals/b5/48/c4/b548c4b1da24ac7ec74785b4e4581d7f.jpg";
+				}
+				this.allBooks.push({
 					title: json.data.docs[i].title_suggest,
-					//publishDate: json.data.docs[i].
-					})
-			/*
-			if(json.data.docs[i].hasOwnProperty( 'isbn')){
-				
-				this.currentISBN = json.data.docs[i].isbn[0];
-				const response = await axios.get('https://openlibrary.org/api/books?bibkeys=ISBN:' + this.currentISBN + '&jscmd=details&format=json');
-				const thePath = "ISBN:" + this.currentISBN;
-				Vue.set(app.allBooks, this.i, new Array);
-				this.allBooks[this.i].push({
-					title: response.data.thePath.details.title,
-					publishDate: response.publish_date,
-					img: response.preview_url
-					})
-				  this.currentISBN=0;
-			}*/
+					publishDate: curDate,
+					author: curAuth,
+					url: curIS,
+					place: i,
+					favorite: false
+				})
+				}
+				this.loading = false;
+			} catch (error) {
+			console.log(error);
+			}
+		},
+		searchBook() {
+			this.allBooks = [];
+			this.findBooks();
+		},  
+		addBooktoFav(bookIndex) {
+			this.allBooks[bookIndex].favorite = true;
+			this.favoiteBooks.push(allBooks[bookIndex]);
 		}
-		this.loading = false;
-	  } catch (error) {
-		console.log(error);
-	  }
-	},
-	searchBook() {
-		this.findBooks();
-  }  
   },
     computed: {
 		
   },
-    watch: {  // will make so changing the number does something
+    watch: { 
   },
 });
